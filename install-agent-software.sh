f4bec1725a68f6a9935583120226e395327b9dfd
JuanPabloVonLandwust <juanpablo.vonlandwust@jala-foundation.org>
2022-10-02 18:40:53 -0400

    Added Edge & FIrefox installation steps

--- install-agent-software.sh
+++ install-agent-software.sh
@@ -3,10 +3,11 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
-# Install Node.js
+# Package names
 nodeSetupFile=nodesource_setup.sh
 chromeSetupFile=google-chrome-stable_current_amd64.deb
-
+edgeSetupFile=microsoft-edge-stable_105.0.1343.53-1_amd64.deb
+# Remove previous versions of the packages
 echo "*** Purging Node.js from the system... ***"
 sudo apt purge -y nodejs
 if [[ -f  /etc/apt/sources.list.d/nodesource.list ]]; then
@@ -14,23 +15,44 @@ if [[ -f  /etc/apt/sources.list.d/nodesource.list ]]; then
 fi
 echo "*** Purging Google Chrome from the system... ***"
 sudo apt purge -y google-chrome-stable
+echo "*** Purging Microsoft Edge from the system... ***"
+sudo apt purge -y microsoft-edge-stable
+echo "*** Purging Mozilla Firefox from the system... ***"
+sudo apt purge -y firefox
 echo "*** Removing no longer required packages... ***"
 sudo apt autoremove -y
 echo "*** Creating directory ~/Downloads... ***"
 mkdir -p ~/Downloads && cd ~/Downloads
+# Install Node.js
 echo "*** Downloading Node.js setup file... ***"
 curl -sL https://deb.nodesource.com/setup_lts.x -o $nodeSetupFile
 sudo bash ~/Downloads/$nodeSetupFile
 echo "*** Installing Node.js... ***"
 sudo apt install -y nodejs
+node --version
+npm --version
 echo "*** Removing Node.js setup file... ***"
 rm ~/Downloads/$nodeSetupFile
+# Install Google Chrome
 echo "*** Downloading Google Chrome setup file... ***"
 wget https://dl.google.com/linux/direct/$chromeSetupFile
 echo "*** Installing Google Chrome... ***"
 sudo apt install -y ~/Downloads/$chromeSetupFile
+google-chrome --version
 echo "*** Removing Google Chrome setup file... ***"
 rm ~/Downloads/$chromeSetupFile
+# Install Microsoft Edge
+echo "*** Downloading Microsoft Edge setup file... ***"
+wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/$edgeSetupFile
+echo "*** Installing Microsoft Edge... ***"
+sudo apt install -y ~/Downloads/$edgeSetupFile
+microsoft-edge --version
+echo "*** Removing Microsoft Edge setup file... ***"
+rm ~/Downloads/$edgeSetupFile
+# Install Mozilla Firefox
+echo "*** Installing Mozilla Firefox... ***"
+sudo apt install -y firefox
+firefox --version
 # Install the Azure Pipelines Agent
 version=$VERSION #$1
 url=$URL #$2
